95396f0cd460cb95f982c02bd175a3c8f12e8deb
Nicholas Emmanuel Chimaobi <nicholasemmanuel321@gmail.com>
2022-06-15 09:12:22 +0100

    fixed linter

--- js/app.js
+++ js/app.js
@@ -19,11 +19,6 @@ const nameValue = document.querySelector('#name');
 const email = document.querySelector('#email');
 const messageValue = document.querySelector('#message');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  validateForm();
-});
-
 const setError = (element, message) => {
   const inputBox = element.parentElement;
   const errorShow = inputBox.querySelector('.error');
@@ -70,3 +65,8 @@ const validateForm = () => {
     setSuccess(messageValue);
   }
 };
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateForm();
+});
